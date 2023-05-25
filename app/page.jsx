5b4556ccafff50c8ada1f35aca44bb2d4fedfaeb
rumon5h <mdrumonkhan48519@gmail.com>
2023-05-25 13:35:00 +0600

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,13 @@ import '../styles/globals.css';
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-        <h1 className='head_text text-center'>Discover & Share</h1>
+        <h1 className='head_text text-center'>Discover & Share 
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'> AI-Powered Prompt</span>
+        </h1>
+        <p className='desc text-center'>Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
     </section>
   )
 }
 
-export default Home
+export default Home;
